5e6f304b2bad381e509dcb543a418c0bca1b42fd
Ganbilly <les1533@gmail.com>
2022-01-31 16:15:03 +0900

    update(18111)

--- C++/silver/2/18111.cpp
+++ C++/silver/2/18111.cpp
@@ -5,13 +5,14 @@
 
 using namespace std;
 
-long long getTime(int **arr, int &n, int &m, long long &avg, long long &b){
+long long getTime(int **arr, int &n, int &m, long long &avg, long long b){
     long long time = 0; 
     long long tmp = 0; 
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             tmp = arr[i][j] - avg;
+            b += tmp;
             if(tmp >= 0){
                 time += tmp * 2;
             }
@@ -21,7 +22,9 @@ long long getTime(int **arr, int &n, int &m, long long &avg, long long &b){
         }
     }
 
-    return time;
+    if(b >= 0)
+        return time;
+    return -1;
 }
 
 bool compare(pair<long long, long long> &a, pair<long long, long long> &b){
@@ -50,13 +53,11 @@ int main(){
         arr[i] = new int[m];
         for(int j = 0; j < m; j++){
             cin >> arr[i][j];
-            avg += arr[i][j];
         }
     }
 
-    avg = round(avg / (double)(n * m));
 
-    for(long long i = avg; i >= 0; i--){
+    for(long long i = 256; i >= 0; i--){
         tmp = getTime(arr, n, m, i, b);
         if( tmp >= 0 ){
             v.push_back({tmp, i});
