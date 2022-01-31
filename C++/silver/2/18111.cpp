0e8acf01022435003575ca05e14e7f22aa497d37
Ganbilly <les1533@gmail.com>
2022-01-31 14:20:11 +0900

    tmp

--- C++/silver/2/18111.cpp
+++ C++/silver/2/18111.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+
+long long getTime(int **arr, int &n, int &m, long long &avg, long long &b){
+    long long time = 0; 
+    long long tmp = 0; 
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            tmp = arr[i][j] - avg;
+            if(tmp >= 0){
+                time += tmp * 2;
+            }
+            else{
+                time += tmp * (-1);
+            }
+        }
+    }
+
+    return time;
+}
+
+bool compare(pair<long long, long long> &a, pair<long long, long long> &b){
+    if (a.first == b.first)
+        return a.second > b.second;
+    return a.first < b.first;
+}
 
 int main(){
 
@@ -8,9 +36,35 @@ int main(){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, m, b;
+    int n, m;
+    long long b;
+    int **arr;
+    long long avg = 0;
+    vector<pair<long long, long long>> v;
+    long long tmp;
 
     cin >> n >> m >> b;
+    arr = new int*[n];
 
+    for(int i = 0; i < n; i++){
+        arr[i] = new int[m];
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
+            avg += arr[i][j];
+        }
+    }
+
+    avg = round(avg / (double)(n * m));
+
+    for(long long i = avg; i >= 0; i--){
+        tmp = getTime(arr, n, m, i, b);
+        if( tmp >= 0 ){
+            v.push_back({tmp, i});
+        }
+    }
+    
+    sort(v.begin(), v.end(), compare);
+
+    cout << v[0].first << ' ' << v[0].second;
     return 0;
 }
