2c9ad9eb1a6de8d01ddc1398ac038085acfd014c
Ganbilly <les1533@gmail.com>
2022-02-22 08:59:37 +0900

    add(02749.py)

---
+++ Python/gold/2/02749.py
@@ -0,0 +1,25 @@
+def mulmat(a, b):
+    result = [[0, 0], [0, 0]]
+    result[0][0] = ((a[0][0] * b[0][0]) + (a[0][1] * b[1][0])) % 1000000
+    result[0][1] = ((a[0][0] * b[0][1]) + (a[0][1] * b[1][1])) % 1000000
+    result[1][0] = ((a[1][0] * b[0][0]) + (a[1][1] * b[1][0])) % 1000000
+    result[1][1] = ((a[1][0] * b[0][1]) + (a[1][1] * b[1][1])) % 1000000
+    
+    return result
+
+def fibo(n):
+    a = [[1, 1], [1, 0]]
+    
+    if n == 1:
+        return a;
+    result = fibo(n//2)
+    result = mulmat(result, result)
+    if n % 2:
+        result = mulmat(result, a)
+    
+    return result
+    
+n = int(input())
+a = fibo(n)
+print(a[0][1])
+
