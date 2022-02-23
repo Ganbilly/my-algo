b47e24b1d38c59e821aadee950d6b14b733768a6
Ganbilly <les1533@gmail.com>
2022-02-23 20:21:57 +0900

    fix(24498.py)

--- Python/silver/4/24498.py
+++ Python/silver/4/24498.py
@@ -1,6 +1,6 @@
 n = int(input())
 a = list(map(int, input().split()))
-ans = max(a)
+ans = max(a[0], a[-1])
 for i in range(1, n - 1) :
     ans = max(ans, a[i] + min(a[i - 1], a[i + 1]))
 print(ans)
