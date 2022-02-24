2df0d9d322b87a5a21672cdb7d0e3a1ed241253e
Ganbilly <les1533@gmail.com>
2022-02-24 22:49:38 +0900

    add(01927.py)

---
+++ Python/silver/2/01927.py
@@ -0,0 +1,22 @@
+import heapq
+
+
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+hq = []
+
+for i in range(n):
+    x = int(input())
+    
+    if x == 0:
+        if len(hq) == 0:
+            print(0)
+        else :
+            print(heapq.heappop(hq))
+    
+    else:
+        heapq.heappush(hq, x)
