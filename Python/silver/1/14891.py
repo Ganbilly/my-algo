38fb59d9f2b007b2c96e44ce304493a2e1e3e3fd
Ganbilly <les1533@gmail.com>
2022-02-28 22:30:04 +0900

    fix(14891.py)

--- Python/silver/1/14891.py
+++ Python/silver/1/14891.py
@@ -1,23 +1,29 @@
 import sys
-import heapq
+from collections import deque
+
+
+def spin(gn, direction):
+    visited[gn] = 1
+    if (gn < 3) and (visited[gn + 1] == 0) and (gear[gn][2] != gear[gn + 1][-2]):
+        spin(gn + 1, direction * -1)
+    if (gn > 0) and (visited[gn - 1] == 0) and (gear[gn][-2] != gear[gn - 1][2]):
+        spin(gn - 1, direction * -1)
+    gear[gn].rotate(direction)
+
+
 input = sys.stdin.readline
+gear = [deque(input().strip()) for _ in range(4)]
+k = int(input())
+spins = [list(map(int, input().split())) for _ in range(k)]
+visited = []
 
-n = int(input())
-count = 0
-h1 = [list(map(int, input().split())) for _ in range(n)]
-h2 = []  
-
-heapq.heapify(h1)
-tmp = heapq.heappop(h1)
-heapq.heappush(h2, [tmp[1], tmp[0]])
-
-while len(h1) != 0:
-    tmp = heapq.heappop(h1)
+for i in range(k):
+    visited = [0] * 4
+    spin(spins[i][0] - 1, spins[i][1])
     
-    while (len(h2) > 0) and (tmp[0] >= h2[0][0]):
-        heapq.heappop(h2)
-    
-    heapq.heappush(h2, [tmp[1], tmp[0]])
-    count = max(count, len(h2))        
+score = 0
 
-print(count)
+for i in range(4):
+    score += int(gear[i][0]) * (1 << i)
+    
+print(score)
